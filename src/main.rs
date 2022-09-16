b8280a3d7698d3c86a840101302e2594927af571
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-16 10:21:19 +0900

    variables

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,11 @@ fn main() {
     /*print!("Hello, ");
     print!("Rust!");*/
     println!("Hello, {}", "students");
+
+    let a: i32 = 1;
+    println!("{}", a);
+
+    let mut b: i32 = 2;
+    b = 3;
+    println!("{}", b)
 }
